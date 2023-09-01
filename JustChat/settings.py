49a570568e1eb7b36d8ca72fdb4f11670d4425c9
Apeksha <apekshashetti1012@gmail.com>
2023-09-01 12:06:41 +0530

    Cors error fixed

--- JustChat/settings.py
+++ JustChat/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     # External
     "drf_spectacular",
     "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -146,3 +148,7 @@ SPECTACULAR_SETTINGS = {
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
